94a49b3a025e3e19bd8b1567a459482f47567f53
Nevold <Nevold@tut.by>
2024-10-03 23:57:35 +0300

    feat: start task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { appendFile, access } from 'node:fs';
+import path, { join } from 'path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = join(__dirname, 'files', 'fresh.txt');
+
+  access(filePath, (err) => {
+    if (err) {
+      appendFile(filePath, 'I am fresh and young', (e) => {
+        if (e) throw err;
+      });
+    } else {
+      throw Error('FS operation failed');
+    }
+  });
 };
 
 await create();
