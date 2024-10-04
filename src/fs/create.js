7e3190b6b0e59ebdd5141a8af1deb66e96669450
Nevold <nevold@tut.by>
2024-10-04 13:15:51 +0300

    fix: fix error message

--- src/fs/create.js
+++ src/fs/create.js
@@ -6,14 +6,15 @@ const create = async () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   const filePath = join(__dirname, 'files', 'fresh.txt');
+  const err = new Error('FS operation failed');
 
-  access(filePath, (err) => {
-    if (err) {
+  access(filePath, (er) => {
+    if (er) {
       appendFile(filePath, 'I am fresh and young', (e) => {
         if (e) throw err;
       });
     } else {
-      throw Error('FS operation failed');
+      console.error(err.message);
     }
   });
 };
