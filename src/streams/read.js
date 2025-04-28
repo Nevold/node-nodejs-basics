2c30fc942332469aaca45840780f869d4498008f
Nevold <nevold@tut.by>
2025-04-28 15:20:27 +0300

    feat: add streams subtasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,30 @@
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+  const readableStream = createReadStream(filePath, 'utf8');
+
+  let data = '';
+
+  const handleError = () => {
+    console.log('FS operation failed');
+    readableStream.destroy();
+  };
+
+  const getData = (chunk) => (data += chunk.toString());
+
+  readableStream
+    .on('data', getData)
+    .on('error', handleError)
+    .on('end', () => {
+      stdout.write(data + '\n');
+    });
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,39 @@
+import { stdout, stdin } from 'node:process';
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  stdout.write('Please, enter text:\n');
+
+  const handleError = () => {
+    console.log('FS operation failed');
+  };
+
+  const handleEnd = (massage) => {
+    console.log(`You entered the exit command:\x1b[33m ${massage}\x1b[0m`);
+    process.exit();
+  };
+
+  const setExitCommand = (chunk) => {
+    if (chunk.toString().trim() == 'exit') {
+      handleEnd('Exit');
+    }
+  };
+
+  const reverse = new Transform({
+    transform(chunk, _, callback) {
+      const reversedText = [...chunk.toString()].reverse().join('') + '\n';
+      this.push(reversedText);
+      callback();
+    },
+  });
+
+  stdin
+    .on('data', (chunk) => setExitCommand(chunk))
+    .on('error', handleError)
+    .pipe(reverse)
+    .pipe(stdout);
+
+  process.on('SIGINT', handleEnd.bind(null, 'Ctrl+C'));
 };
 
 await transform();
