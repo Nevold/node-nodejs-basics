103437601c8875629ae4ba8dac405d3091cd557c
Nevold <Nevold@tut.by>
2025-04-28 21:27:21 +0300

    feat: add hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'node:process';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  const readableStream = createReadStream(filePath);
+  const hash = createHash('sha256');
+
+  const handleError = () => {
+    console.log('FS operation failed');
+    readableStream.destroy();
+  };
+
+  const getData = (chunk) => hash.update(chunk);
+
+  readableStream
+    .on('data', getData)
+    .on('error', handleError)
+    .on('end', () => {
+      stdout.write(hash.digest('hex') + '\n');
+    });
 };
 
 await calculateHash();
