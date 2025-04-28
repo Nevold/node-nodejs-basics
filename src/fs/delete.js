00111b909446b69f1f204481284054f2ff9dda41
Nevold <nevold@tut.by>
2025-04-28 15:17:16 +0300

    feat: add fs subtasks

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { unlink, access, constants } from 'fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+  const err = new Error('FS operation failed');
+
+  try {
+    await access(filePath, constants.F_OK);
+    await unlink(filePath);
+  } catch {
+    console.error(err.message);
+  }
 };
 
 await remove();
